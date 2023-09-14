ca682182caf0137952cf9fa10c7402dcb6b3fe55
Lucasielli <86077823+LucasIelli@users.noreply.github.com>
2023-09-14 21:37:02 +0200

    Need to add verifyAnswer function

--- script.js
+++ script.js
@@ -1,9 +1,103 @@
-const form = Array.from(document.querySelector("form")); // De type tableau, indices étant les inputs
+const questions = [
+  {
+    question: "Quel est le langage de programmation le plus populaire ?",
+    answers: ["JavaScript", "Python", "C++", "Java"],
+    correctAnswer: "JavaScript",
+  },
+  {
+    question: "Quelle est la balise HTML pour créer un paragraphe ?",
+    answers: ["p", "br", "div", "span"],
+    correctAnswer: "p",
+  },
+  {
+    question: "Quelle est la signification de CSS ?",
+    answers: [
+      "Cascading Style Sheet",
+      "Computer Style Sheet",
+      "Colorful Style Sheet",
+      "Creative Style Sheet",
+    ],
+    correctAnswer: "Cascading Style Sheet",
+  },
+  {
+    question:
+      "Quel est le langage de programmation utilisé pour créer des pages web dynamiques/interactives ?",
+    answers: ["HTML", "CSS", "JavaScript", "PHP"],
+    correctAnswer: "JavaScript",
+  },
+  {
+    question:
+      "Quelle balise HTML est utilisée pour créer une liste non ordonnée ?",
+    answers: ["ul", "ol", "li", "dl"],
+    correctAnswer: "ul",
+  },
+  {
+    question: "Quelle est la signification de HTML ?",
+    answers: [
+      "Hyper Text Markup Language",
+      "Hyperlink and Text Markup Language",
+      "Highly Text Markup Language",
+      "Hyper Transfer Markup Language",
+    ],
+    correctAnswer: "Hyper Text Markup Language",
+  },
+  {
+    question: "Quelle est la dernière version de JavaScript ?",
+    answers: ["ES5", "ES6", "ES7", "ES2021"],
+    correctAnswer: "ES2021",
+  },
+  {
+    question: "Quelle balise HTML est utilisée pour créer un lien hypertexte ?",
+    answers: ["a", "link", "href", "url"],
+    correctAnswer: "a",
+  },
+  {
+    question: "Quelle est la principale fonction de CSS ?",
+    answers: [
+      "Gestion des bases de données",
+      "Style et mise en page des pages web",
+      "Programmation de jeux vidéo",
+      "Traitement des paiements en ligne",
+    ],
+    correctAnswer: "Style et mise en page des pages web",
+  },
+  {
+    question: "Quelle est la signification de HTTP ?",
+    answers: [
+      "HyperText Transfer Protocol",
+      "HyperText Testing Protocol",
+      "Hyper Transfer Text Protocol",
+      "Highway Traffic Testing Protocol",
+    ],
+    correctAnswer: "HyperText Transfer Protocol",
+  },
+];
 
-function handleResponse(event) {
-  const label = event.target.nextElementSibling;
-  const labelContent = label.textContent;
-  console.log(labelContent);
+const quizForm = document.getElementById("quiz-form");
+const currentQuestionIndex = 0;
+
+function displayQuestionAnswers(index) {
+  const answersArray = questions[index].answers;
+  quizForm.innerHTML = `
+  <h2>${index + 1}. ${questions[index].question}</h2>
+  ${answersArray
+    .map(
+      (answer, i) =>
+        `
+    <div>
+      <input type="radio" name="answer" id="answer${i + 1}" value="">
+      <label for="answer${i + 1}">${answer}</label>
+    </div>
+  `
+    )
+    .join("")}
+`;
 }
 
-form.addEventListener("click", handleResponse);
+function verifyAnswer(event) {
+  console.log(event.target);
+}
+
+quizForm.addEventListener("click", verifyAnswer);
+
+displayQuestionAnswers(currentQuestionIndex);
